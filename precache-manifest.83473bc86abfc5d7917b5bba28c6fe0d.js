e78ae2a500554adb405deedf11f364294af056d1
Rajagopalan-Gangadharan <g.raju2000@gmail.com>
2020-08-23 14:56:37 +0530

    Updates

---
+++ precache-manifest.83473bc86abfc5d7917b5bba28c6fe0d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57cc6db7bb5706b2675bea6ece913f7",
+    "url": "/JS-OS/index.html"
+  },
+  {
+    "revision": "8a85ffcc5944eb220f44",
+    "url": "/JS-OS/static/css/2.111be65a.chunk.css"
+  },
+  {
+    "revision": "60f78024991d03324ca1",
+    "url": "/JS-OS/static/css/main.94df146e.chunk.css"
+  },
+  {
+    "revision": "8a85ffcc5944eb220f44",
+    "url": "/JS-OS/static/js/2.ead6991d.chunk.js"
+  },
+  {
+    "revision": "60f78024991d03324ca1",
+    "url": "/JS-OS/static/js/main.7464f4d7.chunk.js"
+  },
+  {
+    "revision": "a3fb98936190504393f4",
+    "url": "/JS-OS/static/js/runtime-main.742f714d.js"
+  },
+  {
+    "revision": "6d1e912e21efc36e2497172030a4dfb2",
+    "url": "/JS-OS/static/media/Excel.6d1e912e.svg"
+  },
+  {
+    "revision": "bf105f7d78052fe41253146eef267a34",
+    "url": "/JS-OS/static/media/Particle.bf105f7d.png"
+  },
+  {
+    "revision": "f5fc9ef172c9e8f5e6ec2db3bb31f90c",
+    "url": "/JS-OS/static/media/bgtest.f5fc9ef1.jpg"
+  },
+  {
+    "revision": "b431690e53aacd2d5b0e8693dd565164",
+    "url": "/JS-OS/static/media/browser.b431690e.png"
+  },
+  {
+    "revision": "12177cd52721c1f932e1061f3fb635ed",
+    "url": "/JS-OS/static/media/clock.12177cd5.png"
+  },
+  {
+    "revision": "98a837f1d2aac8ffb765401a88f9111c",
+    "url": "/JS-OS/static/media/file_pdf.98a837f1.png"
+  },
+  {
+    "revision": "2399171c3d14e4e3ba9ba6f4c400b36f",
+    "url": "/JS-OS/static/media/file_txt.2399171c.png"
+  },
+  {
+    "revision": "8542bf873d358ac49ff808b7361a09f5",
+    "url": "/JS-OS/static/media/google.8542bf87.png"
+  },
+  {
+    "revision": "56db4bde67836bb62a0e59eb3296ed42",
+    "url": "/JS-OS/static/media/launcher.56db4bde.png"
+  },
+  {
+    "revision": "9b7457b8b5d91b38e2b6a776cd69e3c6",
+    "url": "/JS-OS/static/media/minesweeper.9b7457b8.png"
+  },
+  {
+    "revision": "7a7e05eac6b7b61afedd3d7eb7a9268e",
+    "url": "/JS-OS/static/media/notepad.7a7e05ea.png"
+  },
+  {
+    "revision": "89609d9d431352f2e33b1f6e3541cd6a",
+    "url": "/JS-OS/static/media/terminal.89609d9d.png"
+  },
+  {
+    "revision": "7e73d334f6ab498f09fa6861d4ab8e7a",
+    "url": "/JS-OS/static/media/vscode.7e73d334.png"
+  }
+]);
