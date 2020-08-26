05b46f3a790a5c813cd51e9c24c0ab845952596c
Rajagopalan-Gangadharan <g.raju2000@gmail.com>
2020-08-26 23:44:02 +0530

    Process launcher kill feature

--- src/defaultapps/processmanager/processmanager.js
+++ src/defaultapps/processmanager/processmanager.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import { FaWind, FaWindowClose } from 'react-icons/fa';
+import { eventShredder } from '../../handlers';
 var sizeof = require('object-sizeof')
 export default class ProcessManager extends Component
 {
@@ -6,6 +9,10 @@ export default class ProcessManager extends Component
     {
         super(props);
         this.state={}
+    }
+    destroy = (event, id) => {
+        console.log(id);
+        eventShredder(id);
     }
     draw = () => {
         console.log("Draw");
@@ -20,11 +27,15 @@ export default class ProcessManager extends Component
         {
             var vx = td[dat[x]];
             var vicon = vx.iconData.substring(2);
+            let id = dat[x];
+            console.log(dat[x]);
             darr.push(
                 <div style={{display:"flex",width:"100%",height:"50px",border:"solid black 1px"}}>
                     <img style={{ margin: "5px", width: "30px", height: "30px" }} src={require('../../desktop/taskbaricon/' + vicon)}></img>
                     <div style={{ margin: "10px" }}>{vx.processName}</div>
-                    <div style={{ marginLeft: "120px",marginTop:"10px" }}>{sizeof(vx)}</div>
+                    <div style={{ marginLeft: "120px", marginTop: "10px" }}>{sizeof(vx)}</div>
+                    <Button style={{ margin: "10px" }} onClick={(event) =>this.destroy(event,id)}
+                    ><FaWindowClose /></Button>
                 </div>
                 //<div><img src={require(`../../desktop/taskbaricon/./icons/browser.png`)}></img></div>
             )
